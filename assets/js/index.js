26f84bab09e60909de0be3f6910cddfb3951031b
RPTecDev <rrpgold@gmail.com>
2024-10-19 00:23:02 -0300

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,17 @@
 /*
-quando clicar no pokedev da listagem temos que esconder o cartão do pokedev aberto e mostrar o cartão correspondente ao que foi selecionado na listagem
+quando clicar no personagem da listagem temos que esconder o cartão do personagem aberto e mostrar o cartão correspondente ao que foi selecionado na listagem
 
 pra isso vamos precisar trabalhar com dois elementos
 1 - listagem
-2 - cartão do pokedev
+2 - cartão do personagem
 
 precisamos criar duas variáveis no JS pra trabalhar com os elementos da tela
 
-vamos precisar trabalhar com um evento de clique feito pelo usuário na listagem de pokedevs
+vamos precisar trabalhar com um evento de clique feito pelo usuário na listagem de personagens
 
 - remover a classe aberto só do cartão que está aberto
 - ao clicar em um pokedev da listagem pegamos o id desse pokedev pra saber qual cartão mostrar
--remover a classe ativo que marca o pokedev selecionado
+-remover a classe ativo que marca o personagem selecionado
 -adicionar a classe ativo no item da lista selecionado 
 */
 
@@ -20,21 +20,21 @@ vamos precisar trabalhar com um evento de clique feito pelo usuário na listagem
 const listaSelecaoPersonagem = document.querySelectorAll('.personagem')
 
 listaSelecaoPersonagem.forEach(personagem => {
-    //vamos precisar trabalhar com um evento de clique feito pelo usuário na listagem de pokedevs
+    //vamos precisar trabalhar com um evento de clique feito pelo usuário na listagem de personagens
 
     personagem.addEventListener('click', () => {
         //remover a classe aberto só do cartão que está aberto
         const cartaoPersonagemAberto = document.querySelector('.aberto')        
         cartaoPersonagemAberto.classList.remove('aberto')
 
-        //ao clicar em um pokedev da listagem pegamos o id desse pokedev pra saber qual cartão mostrar
+        //ao clicar em um personagem da listagem pegamos o id desse personagem pra saber qual cartão mostrar
         const idPersonagemSelecionado = personagem.attributes.id.value
 
         const idDoCartaoPersonagemParaAbrir = 'cartao-' + idPersonagemSelecionado
         const cartaoPersonagemParaAbrir = document.getElementById(idDoCartaoPersonagemParaAbrir)
         cartaoPersonagemParaAbrir.classList.add('aberto')
 
-        //remover a classe ativo que marca o pokedev selecionado
+        //remover a classe ativo que marca o pesonagem selecionado
         const personagemAtivoNaListagem = document.querySelector('.ativo')
         personagemAtivoNaListagem.classList.remove('ativo')
 
